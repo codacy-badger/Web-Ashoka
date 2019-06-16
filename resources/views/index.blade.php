0e42815f60fddc4c617d43f14c4e7b008e27da65
Raviolex <tomasgonzlp@gmail.com>
2019-06-16 18:39:35 -0300

    imagenes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,7 +30,7 @@
       <div class="row">
         <div class="col-sm-12">
           <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5 ">
-            <ul class="list-unstyled list-inline social text-right" style="margin: 0; margin-right:110px">
+            <ul class="list-unstyled list-inline social text-right" style="margin: 0; margin-right:100px">
               <li class="list-inline-item"><a  style="text-decoration:none" href="javascript:void();"><div class="bordes"> <p  class="dona">Doná</p></div> </a></li>
               <li class="list-inline-item"><a href="javascript:void();"> <img src="img/PNGs/Twitter.png"  width="25"alt="twitter"></a></li>
               <li class="list-inline-item"><a href="javascript:void();"> <img src="img/PNGs/Google Plus.png"  width="25"alt="Flowers in Chania"></a></li>
@@ -73,7 +73,7 @@
     </div>
 
        @include('sections.content')
-
+       @include('sections.content2')
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
---
+++ resources/views/sections/content2.blade.php
@@ -0,0 +1,23 @@
+<div class="conteiner"  style="margin-top: 80px"; >
+  <div class="row" style="width:100%;height:350px ; ">
+    <img src="img/PNGs/jovenes.jpg" alt="" style="height:650px ; width:100%;" >
+  </div>
+
+
+  <div class="row"  style="height:170px ;width:100%" >
+    <div class="col-sm-4" style="  padding:0;">
+   <img src="img/PNGs/SANCHEZCABEZAS.jpg" alt="" style="height:100% ; width:100%;" >
+    </div>
+
+  <div class="col-sm-8" style="background-color:#ffae1a; ;padding:0;">
+    <div class="">
+      <p class="text"> "Ashoka esta justo donde debe estar,
+                         nos ofrece andamiaje,un espacio para encontrar
+                          pares e intercambiar experiencias,oportunidades
+                          para dar a conocer y multiplicar lo que hacemos,
+                          espacios para crecer y respetarnos" </p>
+    </div>
+  </div>
+
+  </div>
+  </div>
