69318e5667431898aed3445e4436b079ccbd5572
Raviolex <tomasgonzlp@gmail.com>
2019-06-17 21:04:57 -0300

    emparejando

--- resources/views/sections/content2.blade.php
+++ resources/views/sections/content2.blade.php
@@ -1,27 +1,24 @@
-<div class="conteiner"  style="margin-top: 80px"; >
-  <div class="row" style="width:100%;height:390px ; ">
-    <img src="img/PNGs/jovenes.jpg" alt="" style="height:650px ; width:100%;" >
-  </div>
-
-
-  <div class="row"  style="height:170px ;width:100%" >
-    <div class="col-sm-4" style="  padding:0;">
-   <img src="img/PNGs/SANCHEZCABEZAS.jpg" alt="" style="height:100% ; width:100%;" >
-    </div>
-
-  <div class="col-sm-8 p-5" style="background-color:#ffae1a; ">
-    <div class="">
-      <p class="text-light  m-0   def "> "<strong>Ashoka</strong> esta justo donde debe estar,</p>
-                        <p class="text-light  m-0   def  extrabold " >   nos ofrece andamiaje,un espacio para encontrar</p>
-                      <p class="text-light  m-0  def ">      pares e intercambiar experiencias,oportunidades</p>
-                    <p class="text-light  m-0  def">        para dar a conocer y multiplicar lo que hacemos,</p>
-                    <p class="text-light  def">        espacios para crecer y respetarnos" </p>
-
-
-                          <p class="text-light m-0  def" ><strong>Alejandra Sanches Cabezas.</strong></p>
-                            <p class="text-light m-0  def"><strong>Emprendedora social de la red Ashoka desde 2013</strong></p>
-    </div>
-  </div>
-
+<div class="conteiner mt-5"  style="height:530px ;overflow:hidden"; >
+  <div class="row"  ">
+    <img src="img/PNGs/jovenes.jpg" alt="" style=" width:100%;" >
   </div>
   </div>
+
+
+
+  <div class="conteiner"    >
+    <div class="row  ">
+      <div class="col-sm-4" style="  padding:0;">
+     <img src="img/PNGs/SANCHEZCABEZAS.jpg" alt="" style="height:100% ; width:100%;" >
+      </div>
+      <div class="col-sm-8 p-5" style="background-color:#ffae1a; ">
+         <p class="text-light  m-0   def "> "<strong>Ashoka</strong> esta justo donde debe estar,</p>
+          <p class="text-light  m-0   def  extrabold " >   nos ofrece andamiaje,un espacio para encontrar</p>
+          <p class="text-light  m-0  def ">      pares e intercambiar experiencias,oportunidades</p>
+          <p class="text-light  m-0  def">        para dar a conocer y multiplicar lo que hacemos,</p>
+          <p class="text-light  def">        espacios para crecer y respetarnos" </p>
+          <p class="text-light m-0  def mt-4" ><strong>Alejandra Sanches Cabezas.</strong></p>
+          <p class="text-light m-0  def"><strong>Emprendedora social de la red Ashoka desde 2013</strong></p>
+       </div>
+   </div>
+ </div>
