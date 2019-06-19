8aef0b9f6232727e36f5bb47417f65645e1edcee
Raviolex <36926413+Raviolex@users.noreply.github.com>
2019-06-19 13:45:46 -0300

    Fonts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Ashoka</title>
   <!-- Fonts -->
-    <link href="//fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/estilo.css">
 <link rel="shortcut icon" href="img/PNGs/fav.png" type="image/png">
 
