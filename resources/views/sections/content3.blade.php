91bdcf12796c61598abf054135532fae468a15fb
Raviolex <tomasgonzlp@gmail.com>
2019-06-22 18:21:33 -0300

    galeria

--- resources/views/sections/content3.blade.php
+++ resources/views/sections/content3.blade.php
@@ -1,22 +1,13 @@
-<div class="row p-5">
+<div class="row  py-5   "  style="padding-left: 8rem !important;padding-right: 8rem !important">
   <div class="col-sm-12">
-    <h2 class='m-3'>Novedades</h2>
+    <h2 class='m-3 semiTit'>Novedades</h2>
   </div></div>
-  <div class="row p-5">
-  <div class="col-sm-12">
-    <div class="col-sm-4">
+  <div class="row      h-75 " style="padding-left: 8rem !important;padding-right: 8rem !important">
+    <div class="col-sm-12 col-md-12  col-lg-8  p-0 h-100">
       <img src="/img/PNGs/COMPROMETIDOS.jpg" alt="" style="width:100%;height:100%;">
-    </div>
-    <div class="col-sm-4" >
-<div class="row">
-  <div class="col-sm-12">
-<img src="/img/PNGs/pe.jpg" alt="" style="width:100%;height:100%;">
-  </div>
-</div>
-
-
-
-
-    </div>
-  </div>
-</div>
+        </div>
+    <div class="col-sm-12 col-md-12 col-lg-4    p-0 h-100   " >
+      <img src="/img/PNGs/pe.jpg" alt="" style="width:100%;height:50%;">
+      <img src="/img/PNGs/lc.jpg" alt="" style="width:100%;height:50%;">
+      </div>
+   </div>
