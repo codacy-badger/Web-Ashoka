42b52dbea12708d37bd204ecc74c6eabdd021687
Raviolex <tomasgonzlp@gmail.com>
2019-06-22 18:56:52 -0300

    footerV1

--- resources/views/sections/content3.blade.php
+++ resources/views/sections/content3.blade.php
@@ -2,8 +2,8 @@
   <div class="col-sm-12">
     <h2 class='m-3 semiTit'>Novedades</h2>
   </div></div>
-  <div class="row      h-75 " style="padding-left: 8rem !important;padding-right: 8rem !important">
-    <div class="col-sm-12 col-md-12  col-lg-8  p-0 h-100">
+  <div class="row        " style="padding-left: 8rem !important;padding-right: 8rem !important">
+    <div class="col-sm-12 col-md-12  col-lg-8  p-0  ">
       <img src="/img/PNGs/COMPROMETIDOS.jpg" alt="" style="width:100%;height:100%;">
         </div>
     <div class="col-sm-12 col-md-12 col-lg-4    p-0 h-100   " >
---
+++ resources/views/sections/footer.blade.php
@@ -0,0 +1,36 @@
+<!-- Footer -->
+<footer class=" mt-5 pt-5  " style="background-color: rgb(240, 115, 0);">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!--Grid row-->
+    <div class="row">
+  <div class="col-md-12 text-center">
+     <p class="semiTit">¿Queres recibir informacion,novedades, y oportunidades de Ashoka?</p>
+  </div>
+      <div class="col-md-12 mb-4" >
+
+
+        <form class="form-inline   w-100"   style=" display: inline-block;
+
+      text-align: center;" ">
+          <input class="form-control form-control-sm mr-3 w-50" type="text" placeholder="Ingresa tu mail"
+            aria-label="Search">
+
+        </form>
+        <!-- Form -->
+
+      </div>
+      <!--Grid column-->
+
+
+    </div>
+    <!--Grid row-->
+
+  </div>
+  <!-- Footer Elements -->
+
+
+</footer>
+<!-- Footer -->
