55528e6e7760f5f63af10d825d49e7dc5c9ca839
Raviolex <tomasgonzlp@gmail.com>
2019-06-23 13:47:31 -0300

    metiendoJsPorAbajo
    Solo le mande el hover a la parte de abajo antes del footer

--- resources/views/sections/content3.blade.php
+++ resources/views/sections/content3.blade.php
@@ -3,11 +3,52 @@
     <h2 class='m-3 semiTit'>Novedades</h2>
   </div></div>
   <div class="row        " style="padding-left: 8rem !important;padding-right: 8rem !important">
+
+
     <div class="col-sm-12 col-md-12  col-lg-8  p-0  ">
-      <img src="/img/PNGs/COMPROMETIDOS.jpg" alt="" style="width:100%;height:100%;">
+      <div class="hovereffec">
+        <img class="img-responsive"  src="/img/PNGs/COMPROMETIDOS.jpg"  style="width:100%;height:100%;" alt="">
+        <div class="overla" style="padding-top:25%;">
+          <h2 class="text-left ml-3 "    style="width:15%;">JUVENTUD</h2>
+          <a class="info mt-0" href="#">
+            <h2 >Se lanza</h2>
+            <h2>Comprometidos</h2>
+            <h2>2019</h2>
+          </a>
         </div>
-    <div class="col-sm-12 col-md-12 col-lg-4    p-0 h-100   " >
-      <img src="/img/PNGs/pe.jpg" alt="" style="width:100%;height:50%;">
-      <img src="/img/PNGs/lc.jpg" alt="" style="width:100%;height:50%;">
       </div>
+    </div>
+
+    <div class="col-sm-12 col-md-12 col-lg-4    p-0 h-100   " >
+      <div class="hovereffec">
+        <img class="img-responsive" src="/img/PNGs/pe.jpg" alt="" style="width:100%;height:50%;  ">
+        <div class="overla" style="padding-top:12.5%;">
+          <h2 class="text-left ml-3 "    style="width:30%;">EDUCACION</h2>
+          <a class="info mt-0" href="#"  >
+            <h2 class="h2img">Se presentó</h2>
+            <h2  class="h2img">el documento de </h2>
+            <h2  class="h2img">Primero Educacion</h2>
+          </a>
+        </div>
+      </div>
+      <div class="hovereffec">
+        <img class="img-responsive" src="/img/PNGs/lc.jpg" alt="" style="width:100%;height:50%;  ">
+        <div class="overla" style="padding-top:12.5%;">
+          <h2 class="text-left ml-3 "    style="width:45%;">EMPRENDEDORES SOCIALES</h2>
+          <a class="info mt-0" href="#">
+            <h2  class="h2img">Conviertete en</h2>
+            <h2  class="h2img">un líder de cambio</h2>
+
+          </a>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
    </div>
