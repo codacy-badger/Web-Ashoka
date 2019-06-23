4f628d8669deb5a4986f23fccb52c5b24c670424
Raviolex <tomasgonzlp@gmail.com>
2019-06-23 14:30:49 -0300

    FixCositas

--- resources/views/sections/content3.blade.php
+++ resources/views/sections/content3.blade.php
@@ -1,15 +1,15 @@
-<div class="row  py-5   "  style="padding-left: 8rem !important;padding-right: 8rem !important">
+<div class="row  py-5  cel    "  style=" ">
   <div class="col-sm-12">
     <h2 class='m-3 semiTit'>Novedades</h2>
   </div></div>
-  <div class="row        " style="padding-left: 8rem !important;padding-right: 8rem !important">
+  <div class="row      " style=" ">
 
 
     <div class="col-sm-12 col-md-12  col-lg-8  p-0  ">
       <div class="hovereffec">
         <img class="img-responsive"  src="/img/PNGs/COMPROMETIDOS.jpg"  style="width:100%;height:100%;" alt="">
-        <div class="overla" style="padding-top:25%;">
-          <h2 class="text-left ml-3 "    style="width:15%;">JUVENTUD</h2>
+        <div class="overla pl-3" style="padding-top:25%;">
+          <h2 class="text-left ml-3 "    style="width:19%;">JUVENTUD</h2>
           <a class="info mt-0" href="#">
             <h2 >Se lanza</h2>
             <h2>Comprometidos</h2>
